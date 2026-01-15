39554fc96e45d323a9440900141d25d22bddc84b
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-01-15 23:14:45 +0530

    feat: implement Todo management functionality with context provider

--- todoContextAPi/src/App.jsx
+++ todoContextAPi/src/App.jsx
@@ -1,13 +1,48 @@
 import { useState } from 'react'
+import TodoForm from './components/TodoForm'
+import { TodoContextProvider } from './contexts'
+import TodoItem from './components/TodoItem'
+
 
 
 function App() {
-  
+  const [todos, setTodo] = useState([])
+
+  const addTodo = (todo) => {
+    setTodo((prev) => [...prev, {id: Date.now(), ...todo}])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodo((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodo((prev) => prev.filter((todo) => todo.id !== id ))
+  }
+
+  const toggleCompelete = (id) => {
+    setTodo((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, compeleted: !prevTodo.compeleted}: prevTodo))
+  }
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <TodoContextProvider value = {{todos, addTodo, updateTodo, deleteTodo, toggleCompelete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => {
+              return <TodoItem key={todo.id} todo={todo} />
+            })}
+
+            
+          </div>
+        </div>
+      </div>
+
+    </TodoContextProvider>
   )
 }
 
