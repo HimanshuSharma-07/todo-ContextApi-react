2ee2b21e4bdbccd34359581ce6d7b130573c8616
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-01-17 18:21:27 +0530

    feat: enhance todo management with local storage integration and fix toggle functionality

--- todoContextAPi/src/App.jsx
+++ todoContextAPi/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoForm from './components/TodoForm'
 import { TodoContextProvider } from './contexts'
 import TodoItem from './components/TodoItem'
@@ -6,36 +6,59 @@ import TodoItem from './components/TodoItem'
 
 
 function App() {
-  const [todos, setTodo] = useState([])
+  const [todos, setTodos] = useState([])
 
   const addTodo = (todo) => {
-    setTodo((prev) => [...prev, {id: Date.now(), ...todo}])
+    setTodos((prev) => [...prev, {id: Date.now(),completed: false,...todo}])
   }
 
   const updateTodo = (id, todo) => {
-    setTodo((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? {...prevTodo, ...todo} : prevTodo)))
   }
 
   const deleteTodo = (id) => {
-    setTodo((prev) => prev.filter((todo) => todo.id !== id ))
+    setTodos((prev) => prev.filter((todo) => todo.id !== id ))
   }
 
-  const toggleCompelete = (id) => {
-    setTodo((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, compeleted: !prevTodo.compeleted}: prevTodo))
+  const toggleComplete= (id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo,
+        completed: !prevTodo.completed}: prevTodo))
   }
+
+
+  useEffect(() => {
+   const todos = JSON.parse(localStorage.getItem("todos"))
+   if (todos && todos.length > 0) {
+      setTodos(todos)
+   }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+
+  
 
   return (
-    <TodoContextProvider value = {{todos, addTodo, updateTodo, deleteTodo, toggleCompelete}}>
+    <TodoContextProvider value = {{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             <TodoForm />
           </div>
+          
           <div className="flex flex-wrap gap-y-3">
-            {todos.map((todo) => {
-              return <TodoItem key={todo.id} todo={todo} />
-            })}
+
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className='w-full'>
+                <TodoItem todo={todo}/>
+              </div>
+              
+            ))}
 
             
           </div>
